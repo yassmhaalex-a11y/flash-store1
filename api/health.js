a1a361165eed44f3e26a0a0ab28f8889d8372ff5
yassmhaalex-a11y <yassmha.alex@gmail.com>
2026-09-02 15:24:53 +0300

    Delete api directory

--- api/health.js
+++
@@ -1,8 +0,0 @@
-module.exports=async(req,res)=>{
- try{
-  const db=require('./config');
-  const {error}=await db.from('store_settings').select('id').eq('id',1).single();
-  if(error) throw error;
-  res.status(200).json({ok:true,message:'FLASH STORE API + Supabase connected'});
- }catch(e){res.status(500).json({ok:false,error:e.message})}
-};

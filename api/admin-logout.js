48812547fff89317845156bc88d9a1a6c579ac2f
yassmhaalex-a11y <yassmha.alex@gmail.com>
2026-09-18 15:56:14 +0300

    Delete api directory

--- api/admin-logout.js
+++
@@ -1,6 +0,0 @@
-const {json}=require("./_lib");
-module.exports=async(req,res)=>{
-  if(req.method!=="POST") return json(res,405,{error:"Method not allowed"});
-  res.setHeader("Set-Cookie","flash_admin=; Path=/; HttpOnly; SameSite=Lax; Secure; Max-Age=0");
-  return json(res,200,{ok:true});
-};
